947edf0de536af5d018fdedbe4461a0246b9f49d
Rebecca Allen <rebeccazarsky@gmail.com>
2016-08-19 13:40:17 -0400

    make scripts.js functionally complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 $(document).ready(function() {
   $("#userNumber").submit(function(event) {
     event.preventDefault();
-
     var userValue = parseInt($("input#userInput").val());
-    var ppBall = [userValue];
-    console.log(ppBall);
-
-    for (i=0; i<=userValue; i++) {
-      if (ppBall[i] % 15 === 0) {
+    
+    for (i=1; i<=userValue; i++) {
+      if (i % 15 === 0) {
         $(".results").prepend("<li>pingpong</li>")
       }
-      else if (ppBall[i] % 5 === 0) {
+      else if (i % 5 === 0) {
         $(".results").prepend("<li>pong</li>")
       }
-      else if (ppBall[i] % 3 === 0) {
+      else if (i % 3 === 0) {
         $(".results").prepend("<li>ping</li>")
       }
       else {
-        $(".results").prepend("<li>"[i]"</li>")
+        $(".results").prepend("<li>"+i+"</li>")
       }
     }
   });
