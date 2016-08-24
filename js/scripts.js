5670b704de1ad730728e84bd2ddea4904f9ea0f0
Rebecca Allen <rebeccazarsky@gmail.com>
2016-08-24 16:53:35 -0400

    add separation of business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
+//business logic
+var pingPong = function(userValue) {
+  for (i=1; i<=userValue; i++) {
+    if (i % 15 === 0) {
+      $("#results").append("<li>pingpong</li>");
+    }
+    else if (i % 5 === 0) {
+      $("#results").append("<li>pong</li>");
+    }
+    else if (i % 3 === 0) {
+      $("#results").append("<li>ping</li>");
+    }
+    else {
+      $("#results").append("<li>"+i+"</li>");
+    }
+  }
+}
+//user logic
 $(document).ready(function() {
   $("#userNumber").submit(function(event) {
     event.preventDefault();
     $("#results").empty();
-    var userValue = parseInt($("input#userInput").val());
-    for (i=1; i<=userValue; i++) {
-      if (i % 15 === 0) {
-        $("#results").append("<li>pingpong</li>")
-      }
-      else if (i % 5 === 0) {
-        $("#results").append("<li>pong</li>")
-      }
-      else if (i % 3 === 0) {
-        $("#results").append("<li>ping</li>")
-      }
-      else {
-        $("#results").append("<li>"+i+"</li>")
-      }
-    }
+    var userInput = parseInt($("input#userInput").val());
+    pingPong(userInput);
   });
 });
